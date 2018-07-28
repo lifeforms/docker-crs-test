490df787fc1902e54de10211ebe271217054971b
Walter Hop <walter@lifeforms.nl>
2018-07-28 19:08:16 +0200

    serve.sh: allow just to run a web server with CRS

---
+++ docker/serve.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+[ `whoami` != "root" ] && {
+	echo "This command should only be run from the Docker container."
+	exit 1
+}
+
+# The CRS lives in directory /crs.
+# I bundle the development version of the CRS, but usually, the user
+# will mount their own CRS development directory into /crs instead.
+cd /crs || exit 10
+
+[ ! -f crs-setup.conf.example ] && {
+	echo "Cannot find a CRS directory."
+	exit 11
+}
+
+[ ! -f crs-setup.conf ] && {
+	echo "Creating crs-setup.conf from example..."
+	cp crs-setup.conf.example crs-setup.conf || exit 12
+}
+
+# Get logs into Docker.
+ln -sf /dev/stdout /var/log/apache2/access.log
+ln -sf /dev/stderr /var/log/apache2/error.log
+
+# Don't use our testing configuration for CRS.
+# Overwrite it with a minimal default.
+cat << EOF > /etc/apache2/modsecurity.d/zzz.conf
+# This file has been rewritten by serve.sh
+SecAction "id:900005,\
+  phase:1,\
+  nolog,\
+  pass,\
+  ctl:ruleEngine=On"
+
+Include /crs/*.conf
+Include /crs/rules/*.conf
+EOF
+
+# Start Apache in the foreground.
+apachectl -D FOREGROUND || exit 20
